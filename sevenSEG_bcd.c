f505fb3def34b39f8d7df89c385c60e9188e9cc9
Youssef Sakr <91801319+lankash@users.noreply.github.com>
2021-10-18 23:15:41 +0200

    Add files via upload

---
+++ sevenSEG_bcd.c
@@ -0,0 +1,24 @@
+/*
+
+Code created by "LANKASH"
+  @13/9/2021
+
+File Contents : '7SEG' impelimentation Functions
+
+*/
+
+#include "DIO.h"
+#include "sevenSEG_bsc.h"
+
+void sevenSEG_init_bcd(unsigned char port)    //Initializing the 'O/P' pins for the 7SEG-PCD.
+{
+  DIO_set_pin_dir(port,  BCD_pin,    1);
+  DIO_set_pin_dir(port, (BCD_pin+1), 1);
+  DIO_set_pin_dir(port, (BCD_pin+2), 1);
+  DIO_set_pin_dir(port, (BCD_pin+3), 1);
+}
+
+void sevenSEG_write_bcd(unsigned char port, unsigned char num)
+{
+    DIO_write_nibble(port, num, BCD_nibble);
+} 
